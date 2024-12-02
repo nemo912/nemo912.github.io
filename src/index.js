bdb3cda7a3632fcc0c898872ea8407591ed9e289
nemo <justformygithub@gmail.com>
2024-12-02 10:34:42 +0330

    changes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,63 @@
 import Matter from 'matter-js';
 
-const engine = Matter.Engine.create();
+const { Engine, Render, Runner, Bodies, Composite, Composites, Mouse, MouseConstraint, Constraint } = Matter;
 
-const render = Matter.Render.create({
-    engine: engine,
-    element: document.body,
+const engine = Engine.create({
+    constraintIterations: 1,
+    gravity: {
+        scale: .001,
+    }
 });
 
-const boxA = Matter.Bodies.rectangle(100, 100, 60, 60);
-const line = Matter.Bodies.rectangle(0, 300, 300, 1, {isStatic: true});
+const render = Render.create({
+    engine: engine,
+    element: document.body,
 
-const part1 = Matter.Bodies.rectangle(300, 100, 60, 60, {isStatic: true});
+    options: {
+        width: 1000,
+        height: 500,
+        showPerformance: true,
+    }
+});
+
+const earth = Bodies.rectangle(0, 450, 1500, 10, {isStatic: true});
+const lineA = Bodies.rectangle(0, 300, 300, 10, {isStatic: true});
+const lineB = Bodies.rectangle(900, 300, 300, 10, { isStatic: true });
+
+const boxA = Bodies.rectangle(800, 100, 60, 60);
+const boxB = Bodies.rectangle(920, 100, 60, 60);
+
+const boxesLines = Constraint.create({
+    bodyA: boxA,
+    bodyB: boxB,
+});
+
+const ground = Composites.stack(200, 300, 10, 1, 30, 0, (x, y) => Bodies.rectangle(x, y, 20, 10));
+Composites.chain(ground, 0, 0, 0, 0);
+
+const bridgeJointA = Constraint.create({
+    bodyA: lineA,
+    bodyB: Composite.allBodies(ground)[0],
+    pointA: {x: 150, y: 0},
+});
+
+const bridgeJointB = Constraint.create({
+    bodyA: lineB,
+    bodyB: Composite.allBodies(ground)[9],
+    pointA: {x: -150, y: 0},
+});
 
 
-const bridge = Matter.Composites.stack(200, 100, 3, 1, 30, 0, (x, y) => Matter.Bodies.rectangle(x, y, 60, 60));
-Matter.Composite.add(bridge, part1);
-Matter.Composites.chain(bridge, 0, 0, 0, 0);
+const mouse = Mouse.create(render.canvas);
+const mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+        render: {
+            visible: false,
+        }
+    }
+});
 
-const mouse = Matter.Mouse.create(render.canvas);
-const mc = Matter.MouseConstraint.create(engine, { body: null, mouse: mouse });
-
-
-Matter.Composite.add(engine.world, [boxA, line, bridge, mc]);
-Matter.Render.run(render);
-Matter.Runner.run(Matter.Runner.create(), engine);
+Composite.add(engine.world, [lineA, lineB, earth, mouseConstraint, ground, bridgeJointA, bridgeJointB, boxA, boxB, boxesLines]);
+Render.run(render);
+Runner.run(Runner.create(), engine);
